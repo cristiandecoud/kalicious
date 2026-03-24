0edb0b8e38301d1727a7bb8d8f690dda52f3d321
Cristian Decoud <cristiandecoud@gmail.com>
2026-03-24 19:50:00 -0300

    feat: add service worker implementation with Serwist for caching and precaching

---
+++ app/sw.ts
@@ -0,0 +1,18 @@
+/// <reference lib="webworker" />
+
+import { defaultCache } from "@serwist/next/worker";
+import { Serwist } from "serwist";
+
+declare const self: ServiceWorkerGlobalScope & {
+  __SW_MANIFEST: Array<import("serwist").PrecacheEntry | string>;
+};
+
+const serwist = new Serwist({
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
+});
+
+serwist.addEventListeners();
